de38eba043e0876bad5225a0770661a99738ad31
Alexander Piehl <alexander.piehl@mailbox.org>
2020-06-02 17:45:13 +0200

    initial commit

---
+++ src/test/java/eu/nuoli/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.nuoli.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
